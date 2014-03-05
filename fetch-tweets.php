793afc107b24b17a849923b021acc4f3f14d53e0
miunosoft <miunosoft@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-05 23:06:23 +0000

    Changed the version number to 1.3.5b.
    git-svn-id: https://plugins.svn.wordpress.org/fetch-tweets/trunk@870291 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- fetch-tweets.php
+++ fetch-tweets.php
@@ -5,7 +5,7 @@
 	Description: Fetches and displays tweets from twitter.com with the the Twitter REST API v1.1.
 	Author: miunosoft (Michael Uno)
 	Author URI: http://michaeluno.jp
-	Version: 1.3.4.2b
+	Version: 1.3.5b
 	Requirements: PHP 5.2.4 or above, WordPress 3.3 or above.
 */
 
