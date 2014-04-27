a34bc9accb989a79fa688630efbe6695b0f76a9c
miunosoft <miunosoft@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-27 05:23:09 +0000

    Rephrased an option description.
    git-svn-id: https://plugins.svn.wordpress.org/fetch-tweets/trunk@903332 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- include/class/backend/meta_box/FetchTweets_MetaBox_Template_.php
+++ include/class/backend/meta_box/FetchTweets_MetaBox_Template_.php
@@ -8,7 +8,7 @@ abstract class FetchTweets_MetaBox_Template_ extends FetchTweets_AdminPageFramew
 			array(
 				'field_id'		=> 'fetch_tweets_template',
 				'title'			=> __( 'Select Template', 'fetch-tweets' ),
-				'description'	=> __( 'Sets a default template for this rule. If a template is specified in a widget, the shortcode, or the function, this setting will be overriden.', 'fetch-tweets' ),
+				'description'	=> __( 'Set the default template for this rule. If a template is specified in a widget, the shortcode, or the function, this setting will be overridden.', 'fetch-tweets' ),
 				'label'			=> $arr = $oTemplates->getTemplateArrayForSelectLabel(),
 				'type'			=> 'select',
 				// 'after_field' 	=> '<pre>' . print_r( $arr, true ) . '</pre>', // debug
